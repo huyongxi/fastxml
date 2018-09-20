39e081c45845053f544d0ee5b136e46d37f62e5f
胡永喜.Max <huyongxi.max@snda.com>
2018-09-20 14:21:51 +0800

    update

--- fastxml.h
+++ fastxml.h
@@ -2,13 +2,64 @@
 #define __FASTXML_H__
 #include "rapidxml.hpp"
 #include <string>
+#include <iostream>
 #include <unordered_map>
 
 namespace FastXml {
 
 	using namespace rapidxml;
 
-	class node;
+	class node {
+	private:
+		xml_node<>* _node;
+		char* _name = nullptr;
+		char* _value = nullptr;
+		bool valid = false;
+	public:
+		node(xml_node<>* p) :_node(p){
+			if (_node != nullptr) {
+				valid = true;
+				_name = _node->name();
+				_value = _node->value();
+			}
+		}
+
+		node& operator=(const node&) = delete;
+
+		char* name() { return _name; }
+		char* value() { return _value; }
+
+		auto attribute() { 
+			std::unordered_map<char*, char*> attr_map;
+			if (!*this) {
+				std::cerr << "node not exist don¡¯t have attribute" << std::endl;
+				return attr_map;
+			}
+			for (auto* iter = _node->first_attribute(); iter; iter = iter->next_attribute()) {
+				attr_map.insert(std::make_pair(iter->name(), iter->value()));
+			}
+			return std::move(attr_map); 
+		}
+
+		node operator[](const char* s) {
+			if (!*this) {
+				std::cerr << "operator[]: node not exist!" << std::endl;
+				return node(nullptr);
+			}
+			xml_node<>* child = _node->first_node(s);
+			if (child) {
+				return node(child);
+			}
+			else {
+				std::cerr << _name << " don¡¯t have child node " << s << std::endl;
+				return node(nullptr);
+			}
+		}
+
+		operator bool() {
+			return valid;
+		}
+	};
 
 	class fastxml {
 	private:
@@ -21,56 +72,21 @@ namespace FastXml {
 		bool load_xml(const std::string& filename) {
 			//test
 			data = new char[100];
-			memcpy(data, "<config> <tag type=\"value\" attr=\"qbc\" >hyx  </tag> <t a=\"123\"/> </config>", 72);
+			memcpy(data, "<config> <tag type=\"value\" attr=\"qbc\" >hyx  </tag> <t a=\"123\"/> </config>", 74);
 
 			doc.parse<0>(data);
 			root = doc.first_node();
+			return true;
 		}
 		~fastxml() {
 			delete[] data;
 		}
 
-		fastxml& operator[](const char* name) {
-			static xml_node<>* node;
-			node = doc.first_node("name");
-
-			return *this;
+		node operator[](const char* name) {
+			return node(doc.first_node(name));
 		}
 
 
-	};
-
-	class node {
-	private:
-		xml_node<>* _node;
-		xml_node<>* visit_node;
-		char* _name;
-		char* _value;
-		std::unordered_map<char*, char*> attr_map;
-	public:
-		node(xml_node<>* p) :_node(p), visit_node(nullptr){
-			_name = _node->name();
-			_value = _node->value();
-		}
-		node(const node&) = delete;
-		node& operator=(const node&) = delete;
-		~node() = default;
-
-		char* name() { return _name; }
-		char* value() { return _value; }
-
-		const auto& attribute() { 
-			for (auto* iter = _node->first_attribute(); iter; iter = iter->next_attribute()) {
-				attr_map.insert(std::make_pair(iter->name(), iter->value()));
-			}
-			return attr_map; 
-		}
-
-		node& operator[](const char* s) {
-			static bool first = true;
-			//visit_node = _noe
-			return *this;
-		}
 	};
 }
 
--- test.cpp
+++ test.cpp
@@ -10,21 +10,36 @@ char xml[] = "<config> <tag type=\"value\" attr=\"qbc\" >hyx</tag> <t a=\"123\"/
 
 int main() {
 
-	xml_document<> doc;
-	doc.parse<0>(xml);
-	cout << doc;
-	
-	/*for (xml_node<>* i = doc.first_node(0); i; i = i->first_node(0)) {
-		cout << i->name() << endl;
-	}*/
+	//xml_document<> doc;
+	//doc.parse<0>(xml);
+	//cout << doc;
+	//
+	///*for (xml_node<>* i = doc.first_node(0); i; i = i->first_node(0)) {
+	//	cout << i->name() << endl;
+	//}*/
 
-	xml_node<>* node1 = doc.first_node("config");
-	xml_node<>* node2 = node1->first_node("tag");
-	assert(node2);
-	cout << node2->name() << endl;
-	cout << node2->value() << node2->value_size() << endl;
-	xml_attribute<>* attr = node2->first_attribute("type");
-	cout << attr->name() << attr->value() << endl;
+	//xml_node<>* node1 = doc.first_node("config");
+	//cout << node1->name() << ":" << node1->value() << endl;
+	//xml_node<>* node2 = node1->first_node("tag");
+	//assert(node2);
+	//cout << node2->name() << endl;
+	//cout << node2->value() << node2->value_size() << endl;
+	//xml_attribute<>* attr = node2->first_attribute("type");
+	//cout << attr->name() << attr->value() << endl;
+
+
+	FastXml::fastxml f;
+	f.load_xml("null");
+	auto n1 = f["config"];
+	auto m1 = n1.attribute();
+
+	auto n2 = f["tag"];
+	auto m2 = n2.attribute();
+
+	auto n3 = n1["tag"];
+	auto m3 = n3.attribute();
+
+
 	system("pause");
 	return 0;
 }
